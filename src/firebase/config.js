b11bba4d4622cc0228d1e65a75b8aafab5fd75fe
Francesca Vago <vago.francesca@gmail.com>
2021-04-25 12:16:43 +0200

    add firebase package and config file

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB1tyDD-khCthfP0tNNXUu5Gy3jOY-xzts",
+  authDomain: "image-gallery-3cb34.firebaseapp.com",
+  projectId: "image-gallery-3cb34",
+  storageBucket: "image-gallery-3cb34.appspot.com",
+  messagingSenderId: "1021064853614",
+  appId: "1:1021064853614:web:aa7167779aed3ad7980e7f",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
+   
