33709d4861b43e4ec560908f7f2879e21b90ccaa
Francesca Vago <vago.francesca@gmail.com>
2021-04-25 12:05:32 +0200

    add title component and style

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Title() {
+    return (
+        <div className="title-container">
+            <h1 className="title">Image Gallery</h1>
+        </div>
+    )
+}
